0eb1a27849e3f41ec37e5fe61d26c444828c4703
mallikarjuna-sharma <rmalliksharma96@gmail.com>
2024-03-15 17:36:50 +0530

    base commit

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 var cors = require("cors");
-
 const app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const port = 4000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("server started");
 });
 
 app.post("/login", (req, res) => {
   console.log(req.body);
-  res.send('Success');
+  res.send("Success");
 });
 
 app.listen(port, () => {
